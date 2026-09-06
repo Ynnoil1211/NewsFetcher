c652015eaba027eaff456b1562aee55761fdcefa
Ynnoil1211 <lionnylinli666@gmail.com>
2026-09-06 13:29:08 -0500

    switched to Telegram Bot

--- botService.js
+++ botService.js
@@ -1,13 +1,24 @@
-const TEL = process.env.WHATSAPP_PHONE; //process.env lee las variables dentro de .env
-const API = process.env.CALLMEBOT_API_KEY;
+const TEL = process.env.TELEGRAM_CHAT_ID; //process.env lee las variables dentro de .env
+const API = process.env.TELEGRAM_BOT_TOKEN;
 
-export async function sendWhatsApp(message) {
-  const url = `https://api.callmebot.com/whatsapp.php?phone=${TEL}&text=${encodeURIComponent(message)}&apikey=${API}`;
-  const response = await fetch(url);
+export async function sendTelegram(message) {
+  //usamos POST en vez de
+  // GET (const url = `https://api.telegram.org/bot${API}/sendMessage?chat_id=${TEL}&text=${encodeURIComponent(message)}`;)
+  const url = `https://api.telegram.org/bot${API}/sendMessage`;
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id: TEL,
+      text: message,
+      parse_mode: "Markdown",
+    }),
+  });
   if (response.ok) {
     console.log(`Exitoso ${response.status}`);
   } else {
-    console.log(`Error: ${response.status}`);
+    throw new Error(`Error: ${response.status}`);
   }
-  console.log(`\n${await response.text()}`);
+  const data = await response.json();
+  return data;
 }
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import { getNews } from "./newsService.js";
 import { formatNews } from "./formatter.js";
-import { sendWhatsApp } from "./botService.js";
+import { sendTelegram } from "./botService.js";
 
 try {
   const news = await getNews();
   const final = formatNews(news);
-  if (!final.trim()) throw new Error(`La lista de noticias esta vacia, ${news.length}`);
-  await sendWhatsApp(final);
+  if (!final.trim())
+    throw new Error(`La lista de noticias esta vacia, ${news.length}`);
+  await sendTelegram(final);
 } catch (error) {
   console.error("Error Ocurred." + error);
   process.exit(1);
