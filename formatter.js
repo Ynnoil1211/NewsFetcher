85d8fe27090471530cc46c1c1307a2d3e136b1ad
Ynnoil1211 <lionnylinli666@gmail.com>
2026-09-06 11:29:58 -0500

    note added to formatter.js.

--- formatter.js
+++ formatter.js
@@ -9,15 +9,17 @@ const dic = {
 export function formatNews(results) {
   const filtered = results.filter((item) => item.status === "fulfilled");
   const news = filtered.map((item) => {
-    const { category, articles } = item.value;
-    const categoryName = dic[category];
+    //for each item in filtered news
+    const { category, articles } = item.value; //get category and articles from item
+    const categoryName = dic[category]; //change display name
     const articlesText = articles
-      .slice(0, 2)
+      .slice(0, 2) //only selecting 2 of them
       .map((art) => {
-        return `*${art.title}*\n  ${art.url}`;
+        //for each article of those 2 selected
+        return `*${art.title}*\n  ${art.url}`; //get the article's title + url
       })
-      .join("\n");
-    return `*${categoryName}*\n${articlesText}`;
+      .join("\n"); //join each article together and save them into articlesText
+    return `*${categoryName}*\n${articlesText}`; //return display name + articlesText
   });
   return news.join("\n");
 }
